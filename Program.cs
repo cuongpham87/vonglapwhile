0aef82f3665465f1d7d32dc6367de603ea06547f
cuongpham87 <lil.steven296@gmail.com>
2025-11-05 00:55:13 +0630

    add
    add

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Viết chương trình tính tổng từ 1 đến 10
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Tính tổng các số nguyên từ 1 đến 10
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 10)
+        {
+            sum += i; // cộng dồn giá trị i vào sum
+            i++;      // tăng i để tiến tới 10
+        }
+
+        Console.WriteLine("Tổng từ 1 đến 10 là: " + sum);
+    }
+}
